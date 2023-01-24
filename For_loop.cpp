b0760b5bf96c8d51a73dc23520a6f1b29d71d8f6
MorningStar <113748706+MorningStar2061@users.noreply.github.com>
2023-01-24 18:46:52 +0530

    Add files via upload

---
+++ For_loop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = i; j < 5; j++)
+        {
+            cout << "Hello, Coders!!!" << endl;
+        }
+    }
+    return 0;
+}
